7c1618da95a832a24f93cdef3b8378dead63704f
Sagar Kumar <szkumar16@gmail.com>
2022-10-11 14:47:02 -0400

    first commit

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. Information-Theory-Fall-2022 documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Information-Theory-Fall-2022 documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repository for all code and software developed in relation to the Fall 2022 Directed Study NETS 7976, as well as any work which stems directly from it. Primarily, this is a place to drop useful functions and programs created by the authors in regard to Information Theoretic principles applied to networks or otherwise. ',
+    author='Moritz Laber, Sagar Kumar, Brennan Klein',
+    license='MIT',
+)
